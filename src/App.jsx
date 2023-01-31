50b52f17984a3df9a632746d5d537eab1fef98ae
Trevor Jarrett <txrev91@gmail.com>
2023-01-31 08:17:16 -0500

    Fixed loading state.

--- src/App.jsx
+++ src/App.jsx
@@ -152,9 +152,9 @@ const App = () => {
 
   // Once we have everything, clear our loader and show relevant user interaction items
   useEffect(() => {
-    if (allStarships && manufacturers && allPilotData && allFilmData && page === null) {
+    if (allStarships && manufacturers && allPilotData && allFilmData && page === null && !hasInitialLoad) {
       setHasInitialLoad(true);
-      setStatus('loaded')
+      setStatus('loaded');
     }
   }, [allStarships, manufacturers, allPilotData, allFilmData, page]);
 
@@ -197,13 +197,13 @@ const App = () => {
         />
       }
 
-      {status === 'loading' &&
+      {!hasInitialLoad &&
         <svg className='w-[100px] h-auto animate-spin' xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor">
           <path className='stroke-white' strokeLinecap="round" strokeLinejoin="round" d="M16.023 9.348h4.992v-.001M2.985 19.644v-4.992m0 0h4.992m-4.993 0l3.181 3.183a8.25 8.25 0 0013.803-3.7M4.031 9.865a8.25 8.25 0 0113.803-3.7l3.181 3.182m0-4.991v4.99" />
         </svg>
       }
 
-      {status === 'loaded' && hasInitialLoad &&
+      {hasInitialLoad &&
         <ResultsTable
           results={shipsToUse}
           selectedManufacturer={selectedManufacturer}
